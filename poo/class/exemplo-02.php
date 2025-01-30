c176b59cdc04d926782b498379f40451f6871d76
felipermendess <felipermendes04@gmail.com>
2025-01-29 22:00:54 -0400

    atributos e métodos

---
+++ poo/class/exemplo-02.php
@@ -0,0 +1,44 @@
+<?php 
+class Car{
+    private $model;
+    private $motor;
+    private $year;
+
+    //getters and setters
+    function getModel(){
+        return $this->model;
+    }
+
+    function setModel($model){
+        $this->model = $model;
+    }
+
+    function getMotor():float{
+        return $this->motor;
+    }
+
+    function setMotor($motor){
+        $this->motor = $motor;
+    }
+
+    function getYear():int{
+        return $this->year;
+    }
+
+    function setYear($year){
+        $this->year = $year;
+    }
+
+    function showCar(){
+        return "Model: ".$this->getModel()." | Motor: ".$this->getMotor()." | Year: ".$this->getYear();
+    }
+}
+
+$gol = new Car();
+$gol->setModel("Gol G7");
+$gol->setMotor("1.0");model: 
+$gol->setYear("2018");
+echo $gol->showCar();
+
+
+?>
