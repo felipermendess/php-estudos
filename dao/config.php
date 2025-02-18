747da23a380f78773dfc99c67793abfe1ac7f9ed
felipermendess <felipermendes04@gmail.com>
2025-02-17 22:03:23 -0400

    dao select

--- dao/config.php
+++ dao/config.php
@@ -1,6 +1,6 @@
 <?php 
 spl_autoload_register(function($className){
-    $filename = $className.".php";
+    $filename = "class".DIRECTORY_SEPARATOR.$className.".php";
     if(file_exists($filename)){
         require_once($filename);
     }
