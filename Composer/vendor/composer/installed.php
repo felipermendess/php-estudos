caa70c01c731bbd613e6a8e24f91146efcc22b86
felipermendess <felipermendes04@gmail.com>
2025-03-02 22:47:01 -0400

    composer e exemplo boletoPhp

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'felipermendess/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '949a958a7fe8fd63283a29b876c1e601b1ad48ba',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'felipermendess/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '949a958a7fe8fd63283a29b876c1e601b1ad48ba',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
